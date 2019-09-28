fd90f59e2f1c51b41ecb5fbbdddd88ed4f9cd3db
Greg Vernon <greg.j.vernon@gmail.com>
2019-09-28 15:55:43 -0600

    Add global and local definitions to createElements.m

--- Coding/Assignment_1/src/createElements.m
+++ Coding/Assignment_1/src/createElements.m
@@ -15,4 +15,24 @@ for e = nELEM: -1 : 1
     ELEM(e).LBasisFuns = [];
     ELEM(e).LInterpFun = [];
 end
+
+% Create Global Definitions
+for e = 1:nELEM
+    ELEM(e).GDomain = [nodes(eCONN(1,e)) nodes(eCONN(end,e))];
+    ELEM(e).GNodes  = nodes(eCONN(:,e));
+    ELEM(e).GDOF    = ones(length(ELEM(e).GNodes),1);
+    ELEM(e).GBasisFuns = bFun.basis;
+    ELEM(e).GInterpFun = bFun.basis .* ELEM(e).GDOF;
+end
+
+% Create Local Definitions
+for e = 1:nELEM
+    ELEM(e).LDomain = bFun.domain;
+    ELEM(e).LNodes  = bFun.nodes;
+    ELEM(e).LDOF    = ones(length(ELEM(e).LNodes),1);
+    ELEM(e).LBasisFuns = bFun.basis;
+    ELEM(e).LInterpFun = bFun.basis .* ELEM(e).LDOF;
+end
+
+
 end
