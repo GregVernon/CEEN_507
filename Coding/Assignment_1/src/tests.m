4146cbf5d923bde18f0b6ea7e7d918ef2881f252
Greg Vernon <greg.j.vernon@gmail.com>
2019-09-27 22:21:26 -0600

    Test: linear Lagrange basis function
    References issue #6

--- Coding/Assignment_1/src/tests.m
+++ Coding/Assignment_1/src/tests.m
@@ -15,3 +15,13 @@ assert(isequal(x,[0 1 2 3]));
 [eCONN,x] = generateMesh(0,3,3,2);
 assert(isequal(eCONN,[1 2 3; 3 4 5; 5 6 7]'));
 assert(isequal(x,[0 0.5 1 1.5 2 2.5 3]));
+
+%% Lagrange Basis Functions
+x = sym('x','real');
+bFun = basisFunction("Lagrange",1,x);
+assert(bFun.name == "Lagrange");
+assert(isequal(bFun.degree,1));
+assert(isequal(bFun.variate,x));
+assert(isequal(bFun.domain,[-1 1]));
+assert(isequal(bFun.basis(1), (1-x)/2));
+assert(isequal(bFun.basis(2), (1+x)/2));
