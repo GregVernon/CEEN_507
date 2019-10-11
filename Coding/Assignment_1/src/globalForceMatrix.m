3ddaa5ac928b07a025f8f8239a47e8e563cad7c2
Elizabeth Smith <Liz@pc172-127-11.et.byu.edu>
2019-10-11 17:13:08 -0600

    globalForceMatrix

---
+++ Coding/Assignment_1/src/globalForceMatrix.m
@@ -0,0 +1,9 @@
+function forceMatrix = globalForceMatrix(ELEM)
+for n = 1:nELEM
+    %construct local force vectors
+end
+for n = 1:nElem
+    K = K + k{e} %inputs local matrices into global matrix
+end
+
+end
