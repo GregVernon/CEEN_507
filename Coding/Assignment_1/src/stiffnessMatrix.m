a6b0a2eadfe1d97e67d425287ffdc4e1928eb04b
HannaO <hopdahl@byu.edu>
2019-10-12 15:44:43 -0600

    Add ELEM Jacobian to integrands

--- Coding/Assignment_1/src/stiffnessMatrix.m
+++ Coding/Assignment_1/src/stiffnessMatrix.m
@@ -1,4 +1,4 @@
-function k = stiffnessMatrix(ELEM,eCONN)
+function [K,k] = stiffnessMatrix(ELEM,eCONN)
 nELEM = length(ELEM);
 % Build local matrices for each element
 bFun = ELEM(1).LbFun;
@@ -6,10 +6,12 @@ nLocalNodes = bFun.degree + 1;
 % Exact integration method
 k = sym(zeros(nLocalNodes,nLocalNodes,nELEM));
 for e = 1:nELEM
+    % JAC = ELEM(e).Jacobian_Global_to_LocalVariate;
     for A = 1:nLocalNodes
         dNA = ELEM(e).LDerivBasisFuns(A);
         for B = 1:nLocalNodes
             dNB = ELEM(e).LDerivBasisFuns(B);
+            % k(A,B,e) = int(dNA*dNB*JAC,ELEM(e).LDomain);
             k(A,B,e) = int(dNA*dNB,ELEM(e).LDomain);
         end
     end
@@ -19,11 +21,12 @@ end
 nGlobalNodes = max(max(eCONN));
 K = sym(zeros(nGlobalNodes));
 for e = 1:nELEM
+    JAC = ELEM(e).Jacobian_Global_to_LocalVariate;
     for n1 = 1:nLocalNodes
            for n2 = 1:nLocalNodes
                gID1 = eCONN(n1,e);
                gID2 = eCONN(n2,e);
-               K(gID1,gID2) = K(gID1,gID2) + k(n1,n2,e);
+               K(gID1,gID2) = K(gID1,gID2) + k(n1,n2,e)*JAC;
            end
     end
 end   
