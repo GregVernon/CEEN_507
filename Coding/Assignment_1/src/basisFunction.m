5dde172ab242d1374aff01b05505a388c12708cc
GregVernon <greg.j.vernon@gmail.com>
2019-10-11 19:28:09 -0600

    Add functionality for global basis function construction
    * Explicit construction of global basis & interp functions
    
    * Add some symbolic demos
    
    * Add global <-> local variate mappings
    
    Affine mapping
    
    * Fix local interpoloation functions
    
    * Add clarifying comments to Lagrange basis function routine
    
    * Improve performance
    
    Simple performance improvement by reducing total calls to "sym" and/or "syms"

--- Coding/Assignment_1/src/basisFunction.m
+++ Coding/Assignment_1/src/basisFunction.m
@@ -1,6 +1,6 @@
-function bFun = basisFunction(Family, p, variate)
+function bFun = basisFunction(Family, p, variate,domain)
 if Family == "Lagrange"
-    bFun = lagrangeBasis(p,variate);
+    bFun = lagrangeBasis(p,variate,domain);
 elseif Family == "Legendre"
     bFun = legendreBasis(p,variate);
 end
@@ -10,14 +10,13 @@ end
 % A Simple Expression for Multivariate Lagrange Interpolation
 % Kamron Saniee (c) SIAM, 2007
 % http://evoq-eval.siam.org/Portals/0/Publications/SIURO/Vol1_Issue1/A_Simple_Expression_for_Multivariate.pdf?ver=2018-03-30-130233-050
-function bFun = lagrangeBasis(p,variate)
-domain = [-1 1];
+function bFun = lagrangeBasis(p,variate,domain)
 node = linspace(domain(1), domain(2), p+1);
 basis = sym('L',[p+1,1]);
 
-for ii=1:p+1
+for ii=1:p+1  % ii is the current nodal basis function we're building
     basis(ii)=sym(1);
-    for jj=1:p+1
+    for jj=1:p+1 % jj is evaluating the product series for the current node
         if ii==jj
         else
             basis(ii) = basis(ii)*((variate-node(jj))/(node(ii)-node(jj)));
--- Coding/Assignment_1/src/createElements.m
+++ Coding/Assignment_1/src/createElements.m
@@ -1,4 +1,10 @@
-function ELEM = createElements(eCONN,nodes,bFun)
+function ELEM = createElements(eCONN,nodes,local_bFun)
+
+% Get information about local basis function
+Family = local_bFun.name;
+degree = local_bFun.degree;
+localVariate = local_bFun.variate;
+globalVariate = sym('x','real');
 
 % Preallocate the array of structures
 nELEM = size(eCONN,2);
@@ -8,12 +14,14 @@ for e = nELEM: -1 : 1
     ELEM(e).GDOF = [];
     ELEM(e).GBasisFuns = [];
     ELEM(e).GInterpFun = [];
+    ELEM(e).GlobalVariate_to_LocalVariate = [];
     
     ELEM(e).LDomain = [];
     ELEM(e).LNodes = [];
     ELEM(e).LDOF = [];
     ELEM(e).LBasisFuns = [];
     ELEM(e).LInterpFun = [];
+    ELEM(e).LocalVariate_to_GlobalVariate = [];
 end
 
 % Create Global Definitions
@@ -21,18 +29,34 @@ for e = 1:nELEM
     ELEM(e).GDomain = [nodes(eCONN(1,e)) nodes(eCONN(end,e))];
     ELEM(e).GNodes  = nodes(eCONN(:,e));
     ELEM(e).GDOF    = ones(length(ELEM(e).GNodes),1);
-    ELEM(e).GBasisFuns = bFun.basis;
-    ELEM(e).GInterpFun = bFun.basis .* ELEM(e).GDOF;
+    global_bFun = basisFunction(Family,degree,globalVariate,ELEM(e).GDomain);
+    ELEM(e).GBasisFuns = global_bFun.basis;
+    ELEM(e).GInterpFun = global_bFun.basis' * ELEM(e).GNodes';
 end
 
 % Create Local Definitions
 for e = 1:nELEM
-    ELEM(e).LDomain = bFun.domain;
-    ELEM(e).LNodes  = bFun.nodes;
+    ELEM(e).LDomain = local_bFun.domain;
+    ELEM(e).LNodes  = local_bFun.nodes;
     ELEM(e).LDOF    = ones(length(ELEM(e).LNodes),1);
-    ELEM(e).LBasisFuns = bFun.basis;
-    ELEM(e).LInterpFun = bFun.basis .* ELEM(e).LDOF;
+    ELEM(e).LBasisFuns = local_bFun.basis;
+    ELEM(e).LInterpFun = local_bFun.basis' * ELEM(e).LNodes';
 end
 
+% Create mappings ?(x) <-> x(?)
+for e = 1:nELEM
+    xi_x = computeAffineMapping(ELEM(e).LDomain, ELEM(e).GDomain, globalVariate);
+    x_xi = computeAffineMapping(ELEM(e).GDomain, ELEM(e).LDomain, localVariate);
+    ELEM(e).GlobalVariate_to_LocalVariate = xi_x;
+    ELEM(e).LocalVariate_to_GlobalVariate = x_xi;
+end
 
+end
+
+function F = computeAffineMapping(TargetSpace, Preimage, Domain)
+A = [1 Preimage(1); 1 Preimage(2)];
+b = [TargetSpace(1); TargetSpace(2)];
+c = A\b;
+
+F = c(1) + c(2)*Domain;
 end
