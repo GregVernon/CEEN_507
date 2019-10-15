72a4f7589bed6ee937da8cb6df79050941f5dee8
Greg Vernon <greg.j.vernon@gmail.com>
2019-10-14 23:32:21 -0600

    Assemble approximate solution

---
+++ Coding/Assignment_1/src/assembleSolution.m
@@ -0,0 +1,35 @@
+function [U,ELEM] = assembleSolution(ELEM,eCONN,solutionVector)
+x = sym("x","real");
+d = solutionVector;
+nElem = length(ELEM);
+
+% Insert solution vector into element global degrees of freedom
+for e = 1:nElem
+    nLocalNodes = length(eCONN(:,e));
+    for n = 1:nLocalNodes
+        ELEM(e).GDOF(n) = d(eCONN(n,e));
+    end
+end
+
+% Map element global degrees of freedom into local degrees of freedom
+for e = 1:nElem
+    nLocalNodes = length(eCONN(:,e));
+    for n = 1:nLocalNodes
+        ELEM(e).LDOF(n) = ELEM(e).GDOF(n) * ELEM(e).Jacobian_Local_to_GlobalVariate;
+    end
+end
+
+% Create single piecewise function for ease of plotting
+U = sym(NaN);
+for e = 1:nElem
+    nLocalNodes = length(eCONN(:,e));
+    NA = formula(ELEM(e).GBasisFuns);
+    for n = 1:nLocalNodes
+        if n == 1
+            U = piecewise(ELEM(e).GDomain(1)<=x<ELEM(e).GDomain(2), ELEM(e).GDOF(n) * NA(n),U);
+        else
+            U = piecewise(ELEM(e).GDomain(1)<=x<ELEM(e).GDomain(2), U + ELEM(e).GDOF(n) * NA(n),U);
+        end
+    end
+end
+
