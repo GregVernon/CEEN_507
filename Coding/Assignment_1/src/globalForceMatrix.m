d05a6d525fc3fcdfbe62756e1314601869ee61bb
ElizSmith <55900098+ElizSmith@users.noreply.github.com>
2019-10-15 17:49:41 -0600

    Create global force matrix
    Still in progress

--- Coding/Assignment_1/src/globalForceMatrix.m
+++ Coding/Assignment_1/src/globalForceMatrix.m
@@ -1,9 +1,28 @@
-function forceMatrix = globalForceMatrix(ELEM)
-for n = 1:nELEM
-    %construct local force vectors
-end
-for n = 1:nElem
-    K = K + k{e} %inputs local matrices into global matrix
+function [F,f] = stiffnessMatrix(ELEM,eCONN,fun)
+nELEM = length(ELEM);
+% Build local matrices for each element
+bFun = ELEM(1).LbFun;
+nLocalNodes = bFun.degree + 1;
+% Exact integration method
+f = sym(zeros(nLocalNodes,nELEM))
+DNB = ELEM(nLocalNodes).LDerivBasisFuns(nLocalNodes)
+for e = 1:nELEM
+    % JAC = ELEM(e).Jacobian_Global_to_LocalVariate;
+    for A = 1:nLocalNodes
+        NA = ELEM(e).LBasisFuns(A);
+        DNA = ELEM(e).LDerivBasisFuns(A);
+        f(A,e) = int(fun*NA,ELEM(e).LDomain)+NA(0)*h - int(DNA*DNB,ELEM(e).LDomain)*g;
+        %Figure out h and g
+        end
+    end
 end
 
+nGlobalNodes = max(max(eCONN));
+F = sym(zeros(nGlobalNodes));
+for e = 1:nELEM
+    JAC = ELEM(e).Jacobian_Global_to_LocalVariate;
+    for n1 = 1:nLocalNodes
+         gID1 = eCONN(n1,e);
+            F(gID1) = F(gID1) + f(n1,e)*JAC; %input local values into global force vector
+    end
 end
