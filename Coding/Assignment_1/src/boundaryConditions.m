4e4152af19bf012d9ba1a1e78a6243c053cc57e8
Greg Vernon <greg.j.vernon@gmail.com>
2019-10-14 23:32:32 -0600

    Apply boundary conditions

---
+++ Coding/Assignment_1/src/boundaryConditions.m
@@ -0,0 +1,46 @@
+function [K,F,BC] = boundaryConditions(K,F,BC,ELEM,eCONN,nodes)
+
+g = BC.U.val;
+gLoc = BC.U.location;
+gNode = find(isAlways(subs(gLoc,lhs(gLoc), nodes)));
+BC.U.Node = gNode;
+
+h = BC.dU.val;
+hLoc = BC.dU.location;
+hNode = find(isAlways(subs(hLoc,lhs(hLoc), nodes)));
+BC.dU.Node = hNode;
+
+nElem = length(ELEM);
+for e = 1:nElem
+    JAC = ELEM(e).Jacobian_Global_to_LocalVariate;
+    nLocalNodes = length(ELEM(e).LNodes);
+    
+    % Apply g
+    [isInElement,idx] = ismember(gNode,eCONN(e));
+    if isInElement == true
+        dNg = ELEM(e).LDerivBasisFuns(idx);
+    else
+        dNg = sym(0);
+    end
+    
+    for n = 1:nLocalNodes
+        dNA = ELEM(e).LDerivBasisFuns(n);
+        gID = eCONN(n,e);
+        F(gID) = F(gID) - int(dNA * dNg,ELEM(e).LDomain) * JAC;
+    end
+    
+    % Apply h
+    [isInElement,idx] = ismember(hNode,eCONN(e));
+    if isInElement == true
+        gID = eCONN(idx,e);
+        N = ELEM(e).LBasisFuns(rhs(hLoc));
+        NA = N(idx);
+        F(gID) = F(gID) + (NA * h) * JAC;
+    end
+end
+
+K(gNode,:) = [];
+K(:,gNode) = [];
+F(gNode) = [];
+
+end
