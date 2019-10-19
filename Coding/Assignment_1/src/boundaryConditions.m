78c1233d70ee8d7e006ed75c0f999aab204fe10e
Greg Vernon <greg.j.vernon@gmail.com>
2019-10-19 16:31:43 -0600

    Fix boundary conditions application

--- Coding/Assignment_1/src/boundaryConditions.m
+++ Coding/Assignment_1/src/boundaryConditions.m
@@ -1,62 +1,73 @@
-boundaryCondition.g;
-boundaryCondition.h;
-boundaryCondition.gBCNode;
-boundaryCondition.gELEM;
-
+function [K, F, BC] = boundaryConditions(K, F, BC, ELEM, eCONN, nodes)
 %% Incorporate Boundary Conditions
-function [K, F, BC] = boundaryCondition(K, F, BC, ELEM, eCONN, nodes);
 
 % Create matrix of d variables
+nNodes = length(nodes);
 d = sym('d', [nNodes 1]);
 
 % Define Dirichlet (traction) and Neumann (natural) BCs
-g = boundaryCondition.g;
-gBCNode = boundaryCondition.gBCNode;
-gELEM = boundaryCondition.gELEM;
-nLocalNodes = bFun.degree + 1;
+
+% Extract info for "g" (solution BC)
+g = BC.U.val;
+gLoc = BC.U.location;
+% gNodeID -> Global Node ID
+BC.U.gNodeID = find(isAlways(subs(gLoc,lhs(gLoc), nodes)));
 
 % Subtract the g terms from the F matrix
+nELEM = length(ELEM);
 for e = 1:nELEM
-    % Jac_L2G = ELEM(e).Jacobian_Local_to_GlobalVariate;
-    dNG = ELEM(gELEM).GbFun(G);
+    % Apply g
+    [isInElement,idx] = ismember(BC.U.gNodeID,eCONN(:,e));
+    if isInElement == true
+        dNG = ELEM(e).LDerivBasisFuns(idx);
+    else
+        dNG = sym(0);
+    end
+    
+    dNe = formula(ELEM(e).LBasisFuns);
+    JAC = ELEM(e).Jacobian_Local_to_GlobalVariate;
+    nLocalNodes = length(ELEM(e).LNodes);
     for A = 1:nLocalNodes
-        dNA = ELEM(e).GbFun(A);
-        gTerm1 = int(dNA*dNG,ELEM(e).GDomain)*g;
-        F(A) = F(A) - gTerm1;
-        for B = 1:nLocalNodes
-            dNB = ELEM(e).GbFun(B);
-            hTerm2 = int(dNB*dNG,ELEM(e).GDomain)*g;
-            F(B) = F(B) - hTerm2;
-        end
+        dNA = dNe(A);
+        gTerm = int(dNA*dNG,ELEM(e).LDomain)*g;
+        F(A) = F(A) - gTerm*JAC; % VERIFY
     end
 end
+
+
+% Extract info for "h" (derivative of solution BC)
+h = BC.dU.val;
+hLoc = BC.dU.location;
+% gNodeID -> Global Node ID
+BC.dU.gNodeID = find(isAlways(subs(hLoc,lhs(hLoc), nodes)));
+
 
 % Subtract the h terms from the F matrix
 for e = 1:nELEM
-    Jac_L2G = ELEM(e).Jacobian_Local_to_GlobalVariate;
-   for A = 1:nLocalNodes
-        NA = ELEM(e).LbFun(0)*Jac_L2G;
-        hTerm1 = NA*h;
-        F(A) = F(A) - hTerm1;
-        for B = 1:nLocalNodes
-            NB = ELEM(e).LbFun(0)*Jac_L2G;
-            hTerm2 = NB*h;
-            F(B) = F(B) - hTerm2;
-        end
+    %     Jac_L2G = ELEM(e).Jacobian_Local_to_GlobalVariate;
+    Ne = formula(ELEM(e).LBasisFuns);
+    % Apply h
+    [isInElement,idx] = ismember(BC.dU.gNodeID,eCONN(:,e));
+    if isInElement == true
+        NH = Ne(idx);
+    else
+        NH = sym(0);
     end
+    JAC = ELEM(e).Jacobian_Local_to_GlobalVariate;
+    hTerm = NH*h; % VERIFY
+    F(BC.dU.gNodeID) = F(BC.dU.gNodeID) - hTerm*JAC;
 end
-
 % Reconstruct new d matrix with g-value inserted at correct node
 % d = [d(1:gBCNode-1); g; d(gBCNode+1:1)];
 
 % Reconstruct new F and d matrices that subtract the gNode terms
 % F = F - K(:,gBCNode).*d(gBCNode);
-F(gBCNode) = [];
-d(gBCNode) = [];
+F(BC.U.gNodeID) = [];
+d(BC.U.gNodeID) = [];
 
 % Reconstruct K matrix by removing all terms associated with g BC
-K(gBCNode,:) = [];
-K(:,gBCNode) = [];
+K(BC.U.gNodeID,:) = [];
+K(:,BC.U.gNodeID) = [];
 
 % Define the Kd=F relationship
 K * d == F;
