fa45a4bed435bc6073e2e63b4b4d10f6dee87599
HannaO <hopdahl@byu.edu>
2019-10-19 11:14:31 -0600

    Changed BC code to loop through elements

--- Coding/Assignment_1/src/boundaryConditions.m
+++ Coding/Assignment_1/src/boundaryConditions.m
@@ -1,23 +1,63 @@
 boundaryCondition.g;
 boundaryCondition.h;
-boundaryCondition.gNode;
+boundaryCondition.gBCNode;
+boundaryCondition.gELEM;
 
-%% Incorpoorate Boundary Conditions
+%% Incorporate Boundary Conditions
 function [K, F, BC] = boundaryCondition(K, F, BC, ELEM, eCONN, nodes);
+
 % Create matrix of d variables
 d = sym('d', [nNodes 1]);
+
 % Define Dirichlet (traction) and Neumann (natural) BCs
 g = boundaryCondition.g;
-gNode = boundaryCondition.gNode;
+gBCNode = boundaryCondition.gBCNode;
+gELEM = boundaryCondition.gELEM;
+nLocalNodes = bFun.degree + 1;
+
+% Subtract the g terms from the F matrix
+for e = 1:nELEM
+    % Jac_L2G = ELEM(e).Jacobian_Local_to_GlobalVariate;
+    dNG = ELEM(gELEM).GbFun(G);
+    for A = 1:nLocalNodes
+        dNA = ELEM(e).GbFun(A);
+        gTerm1 = int(dNA*dNG,ELEM(e).GDomain)*g;
+        F(A) = F(A) - gTerm1;
+        for B = 1:nLocalNodes
+            dNB = ELEM(e).GbFun(B);
+            hTerm2 = int(dNB*dNG,ELEM(e).GDomain)*g;
+            F(B) = F(B) - hTerm2;
+        end
+    end
+end
+
+% Subtract the h terms from the F matrix
+for e = 1:nELEM
+    Jac_L2G = ELEM(e).Jacobian_Local_to_GlobalVariate;
+   for A = 1:nLocalNodes
+        NA = ELEM(e).LbFun(0)*Jac_L2G;
+        hTerm1 = NA*h;
+        F(A) = F(A) - hTerm1;
+        for B = 1:nLocalNodes
+            NB = ELEM(e).LbFun(0)*Jac_L2G;
+            hTerm2 = NB*h;
+            F(B) = F(B) - hTerm2;
+        end
+    end
+end
+
 % Reconstruct new d matrix with g-value inserted at correct node
-d = [d(1:gNode-1); g; d(gNode+1:1)];
-% Reconstruct new F and d matrices that subtracts the gNode terms
-F = F - K(:,gNode).*d(gNode);
-F(gNode) = [];
-d(gNode) = [];
+% d = [d(1:gBCNode-1); g; d(gBCNode+1:1)];
+
+% Reconstruct new F and d matrices that subtract the gNode terms
+% F = F - K(:,gBCNode).*d(gBCNode);
+F(gBCNode) = [];
+d(gBCNode) = [];
+
 % Reconstruct K matrix by removing all terms associated with g BC
-K(gNode,:) = [];
-K(:,gNode) = [];
+K(gBCNode,:) = [];
+K(:,gBCNode) = [];
+
 % Define the Kd=F relationship
 K * d == F;
 end
