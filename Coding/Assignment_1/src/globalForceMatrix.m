d28a65ddb8b95b137cacabbe8d9129ec6da7effd
Greg Vernon <greg.j.vernon@gmail.com>
2019-10-19 15:28:27 -0600

    Fix mappings (Local - Global)

--- Coding/Assignment_1/src/globalForceMatrix.m
+++ Coding/Assignment_1/src/globalForceMatrix.m
@@ -8,21 +8,21 @@ f = sym(zeros(nLocalNodes,nELEM))
 DNB = ELEM(nLocalNodes).LDerivBasisFuns(nLocalNodes)
 for e = 1:nELEM
     % JAC = ELEM(e).Jacobian_Global_to_LocalVariate;
-    for A = 1:nLocalNodes
-        NA = ELEM(e).LBasisFuns(A);
+    Ne = formula(ELEM(e).LBasisFuns);
+    for A = 1:nLocalNodes  
+        NA = Ne(A);
         DNA = ELEM(e).LDerivBasisFuns(A);
-        f(A,e) = int(fun*NA,ELEM(e).LDomain)+NA(0)*h - int(DNA*DNB,ELEM(e).LDomain)*g;
+        f(A,e) = int(NA * (fun(ELEM(e).LocalVariate_to_GlobalVariate)), ELEM(e).LDomain);
         %Figure out h and g
-        end
     end
 end
 
 nGlobalNodes = max(max(eCONN));
 F = sym(zeros(nGlobalNodes, 1));
 for e = 1:nELEM
-    JAC = ELEM(e).Jacobian_Global_to_LocalVariate;
+    JAC = ELEM(e).Jacobian_Local_to_GlobalVariate;
     for n1 = 1:nLocalNodes
-         gID1 = eCONN(n1,e);
-            F(gID1) = F(gID1) + f(n1,e)*JAC; %input local values into global force vector
+        gID1 = eCONN(n1,e);
+        F(gID1) = F(gID1) + f(n1,e)*JAC; %input local values into global force vector
     end
 end
