042216a1c76bd7910953e968cb611220f37f7df6
Greg Vernon <greg.j.vernon@gmail.com>
2019-10-21 16:32:58 -0600

    Formatting

--- Coding/Assignment_1/src/main.m
+++ Coding/Assignment_1/src/main.m
@@ -1,10 +1,10 @@
 function feSolution = main(nElem, elemDegree, f, g, h)
-% Define problem domain
+%% Define problem domain
 x = sym("x","real");
 xMin = 0;
 xMax = 1;
 
-% Define Load-Case
+%% Define Load-Case
 %%%%% Example inputs
 % if loadCase == 1
 %     q = sym(1);
@@ -15,7 +15,7 @@ xMax = 1;
 %     f(x) = x^2;
 % end
 % 
-% % Define boundary conditions
+%% Define boundary conditions
 % g = sym(1); 
 % h = sym(0.25);
 
@@ -24,26 +24,26 @@ BC.U.val = g;
 BC.dU.location = x == 0;
 BC.dU.val = h;
 
-% Construct the finite element space
+%% Construct the finite element space
 [eCONN,nodes] = generateMesh(xMin,xMax,nElem,elemDegree);
 bFun = basisFunction("Lagrange", elemDegree, sym("xi","real"), [-1 1]);
 ELEM = createElements(eCONN,nodes,bFun);
 
-% Construct linear system of equations
+%% Construct linear system of equations
 K = stiffnessMatrix(ELEM,eCONN,"GaussQuadrature");
 F = forceVector(ELEM,eCONN,f,"GaussQuadrature");
 
-% Apply boundary conditions
+%% Apply boundary conditions
 [K,F,BC] = boundaryConditions(K,F,BC,ELEM,eCONN,nodes,"GaussQuadrature");
 
-% Solve the system of equations
+%% Solve the system of equations
 d = K\F;
 d = [d(1:(BC.U.gNodeID-1)); BC.U.val; d(BC.U.gNodeID:end)];
 
-% Assemble the solution
+%% Assemble the solution
 [U,ELEM] = assembleSolution(ELEM,eCONN,d);
 
-% Output results
+%% Output results
 feSolution.Elements = ELEM;
 feSolution.Mesh.Connectivity = eCONN;
 feSolution.Mesh.Nodes = nodes;
