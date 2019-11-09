42062c50ae50034c3f0004ce741435ef1b834904
HannaO <hopdahl@byu.edu>
2019-11-09 12:20:57 -0700

    Update cantilever function for deflection as a function of length

--- Coding/Assignment_2/src/cantileverDeflection.m
+++ Coding/Assignment_2/src/cantileverDeflection.m
@@ -3,3 +3,9 @@ I = (b * h^3) / 12;
 f = 10 * h^3;
 u = (f * L^4) / (8 * E * I);
 end
+
+function u(x) = cantileverXDeflection(h, b, L, E)
+I = (b * h^3) / 12;
+f = 10 * h^3;
+u(x) = (f * x^2) / (24 * E * I) * (x^2 + 6 * L^2 - 4*L*x);
+end
