bb290672563384b04828f1ec57a6f190793a3ac2
Greg Vernon <greg.j.vernon@gmail.com>
2019-11-10 22:03:23 -0700

    Compute exact solution of Euler-Bernoulli problem

--- Coding/Assignment_2/src/computeExactSolution.m
+++ Coding/Assignment_2/src/computeExactSolution.m
@@ -1,8 +1,10 @@
-function exactSolution = computeExactSolution(f,g,h)
+function exactSolution = computeExactSolution(E,I,f,g,h,m,q)
 syms u(x)
-eqn = diff(u,x,2) + f == 0;
+eqn = E*I*diff(u,x,4) + f == 0;
 Du = diff(u,x);
-cond = [u(1)==g, Du(0)==h];
+D2u = E*I*diff(u,x,2);
+D3u = E*I*diff(u,x,3);
+cond = [u(1)==g, Du(1)==h, D2u(0)==m, D3u(0)==q];
 U(x) = dsolve(eqn,cond);
 
 exactSolution.f = f;
