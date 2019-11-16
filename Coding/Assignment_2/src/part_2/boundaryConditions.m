b5ce8177363e3023340858fdfeac408ec9277b81
Greg Vernon <greg.j.vernon@gmail.com>
2019-11-15 21:28:11 -0700

    Shear term doesn't need Jacobian mapping

--- Coding/Assignment_2/src/part_2/boundaryConditions.m
+++ Coding/Assignment_2/src/part_2/boundaryConditions.m
@@ -132,8 +132,8 @@ for e = 1:nELEM
             if n == idx
                 Nh = Ne(n);
                 Nh = symfun(Nh,symvar(Nh)); % Turn into symbolic function
-                qTerm = Nh(ELEM(e).LNodes(n)) * q * JAC;
                 F(BC.d3U.gNodeID) = F(BC.d3U.gNodeID) + qTerm;
+                qTerm = Nh(ELEM(e).LNodes(n)) * q;
             end
         end
     end
