95b4d79aba2ee2f4b4737f51661d4cdea6d00433
ElizSmith <55900098+ElizSmith@users.noreply.github.com>
2019-11-25 15:42:54 -0700

    Filled out B

--- Coding/Assignment_3/src/strainDisplacementMatrix.m
+++ Coding/Assignment_3/src/strainDisplacementMatrix.m
@@ -1,9 +1,15 @@
 function B = strainDisplacementMatrix(N)
+%Make B a part of the basis function structure?
 
 % Preallocate
 B = sym(zeros(6,6));
-
-B(2,1) = diff(N,symvar(N));
-% continue filling the rest of B
-
+val = diff(N,symvar(N)); %compute diff once
+B(2,1) = val;
+B(3,2) = val;
+B(1,3) = val;
+B(3,4) = N;
+B(4,4) = val;
+B(2,5) = -N;
+B(5,5) = val;
+B(6,6) = val;
 end
