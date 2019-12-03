e3a3399d9dd59542d18a4b94c2eeeda0819d55e2
Greg Vernon <greg.j.vernon@gmail.com>
2019-12-02 22:06:46 -0700

    Exact solution for uniform x-section Timoshenko beams

--- Coding/Assignment_3/src/computeExactSolution.m
+++ Coding/Assignment_3/src/computeExactSolution.m
@@ -1,16 +1,27 @@
-function exactSolution = computeExactSolution(E,I,f,g,h,m,q)
+function exactSolution = computeExactSolution(L,E,I,k,A,G,f,conditions)
 syms u(x)
-eqn = diff(u,x,4) == f/(E*I);
-Du = diff(u,x);
-D2u = diff(u,x,2);
-D3u = diff(u,x,3);
-cond = [u(0)==g, Du(0)==h, D2u(1)==m/(E*I), D3u(1)==q/(E*I)];
+eqn = diff(u,x,4) == f/(E*I) - 1/(k*A*G) * diff(f,x,2);
+
+% conditions = [deriv_1, x_1, val_1;...
+%               deriv_2, x_2, val_2;...
+%               deriv_3, x_3, val_3;...
+%               deriv_4, x_4, val_4]
+
+assert(isequal(size(conditions),[4,3]))
+cond = sym(zeros(1,4));
+for ii = 1:size(conditions,1)
+    deriv_i = conditions(ii,1);
+    x_i = conditions(ii,2);
+    val_i = conditions(ii,3);
+    du = diff(u,x,deriv_i);
+    cond(ii) = du(x_i)==val_i;
+end
 U(x) = dsolve(eqn,cond);
 
 exactSolution.f = f;
 exactSolution.eqn = eqn;
 exactSolution.cond = cond;
-exactSolution.domain = [0 1];
+exactSolution.domain = [0 L];
 exactSolution.U = U;
 
 end
