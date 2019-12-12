b7395328a7106724dd77d6edcad0e054449dd941
ElizSmith <55900098+ElizSmith@users.noreply.github.com>
2019-12-12 15:56:03 -0700

    Update AssembleSolution6DOF.m

--- Coding/Assignment_3/src/AssembleSolution6DOF.m
+++ Coding/Assignment_3/src/AssembleSolution6DOF.m
@@ -1,4 +1,8 @@
-function [U,ELEM] = AssembleSolution6DOF(ELEM, BSpline, ECONN)
+function [U,ELEM] = AssembleSolution6DOF(numElem, ELEM, BSpline, ECONN)
 %iterate to assemble global stiffness and force matrices from element matrices
+ndof = 6; % 6 Degrees of Freedom
+for a = 1:numElem
+  for idof = 1:ndof
+    if K[e,a,idof]
 
 end
