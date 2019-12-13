f51d6dcd6660247b01733f70105cf05f4cbbaf4a
Greg Vernon <greg.j.vernon@gmail.com>
2019-12-12 17:16:48 -0700

    Update main function arguments, remove dead code

--- Coding/Assignment_3/src/main.m
+++ Coding/Assignment_3/src/main.m
@@ -1,35 +1,8 @@
-function feSolution = main(nElem, elemDegree, elemContinuity, f, g, h, m, q, EI)
+function feSolution = main(nElem, elemDegree, elemContinuity, f, BC, base, height, E, nu, shearCorrection)
 %% Define problem domain
 x = sym("x","real");
 xMin = 0;
 xMax = 1;
-
-%% Define Load-Case
-%%%%% Example inputs
-% if loadCase == 1
-%     q = sym(1);
-%     f(x) = q;
-% elseif loadCase == 2
-%     f(x) = x;
-% elseif loadCase == 3
-%     f(x) = x^2;
-% end
-% 
-%% Define boundary conditions
-% g = sym(1); 
-% h = sym(0.25);
-
-BC.U.location = x == 0;
-BC.U.val = g;
-
-BC.dU.location = x == 0;
-BC.dU.val = h;
-
-BC.d2U.location = x == 1;
-BC.d2U.val = m;
-
-BC.d3U.location = x == 1;
-BC.d3U.val = q;
 
 %% Construct the finite element Spline Space
 % Create the B-Spline
