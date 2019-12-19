da1e643cd6405f46289f2925b620639c9968f33f
Greg Vernon <greg.j.vernon@gmail.com>
2019-12-19 11:18:45 -0600

    Update boundaryConditions.m

--- Coding/Assignment_3/src/boundaryConditions.m
+++ Coding/Assignment_3/src/boundaryConditions.m
@@ -4,29 +4,28 @@ eCONN = BSpline.elementConnectivity;
 C = BSpline.decomposition.localExtractionOperator;
 nodes = BSpline.nodes;
 nELEM = length(C);
-
+nDOF = size(ELEM(1).G_D,1);
 
 % Node->DOF mapping
-DOFS = fieldnames(BC);
-nDOF = length(DOFS);
+bcDOFS = fieldnames(BC);
+nBCDOF = length(bcDOFS);
 nGlobalNodes = max(max(eCONN));
 nodeDOFS = reshape(1:nGlobalNodes*nDOF,nDOF,nGlobalNodes);
 
 % Create matrix of d variables
-nGlobalNodes = length(nodes);
 d = sym('d', [max(nodeDOFS(:)) 1]);
 
 % Define Dirichlet (traction) and Neumann (natural) BCs
 %% Dirchlet BCs ( u(x), v(x), w(x) )
-for ii = 1:6
-    val = BC.(DOFS{ii}).val;
-    Loc = BC.(DOFS{ii}).location;
-    [BC.(DOFS{ii}).globalNodeID,~,~] = find(isAlways(subs(Loc,symvar(Loc), nodes)));
-    BC.(DOFS{ii}).globalDOF = nodeDOFS(ii,BC.(DOFS{ii}).globalNodeID);
+for ii = 1:nBCDOF
+    val = BC.(bcDOFS{ii}).val;
+    Loc = BC.(bcDOFS{ii}).location;
+    [BC.(bcDOFS{ii}).globalNodeID,~,~] = find(isAlways(subs(Loc,symvar(Loc), nodes)));
+    BC.(bcDOFS{ii}).globalDOF = nodeDOFS(ii,BC.(bcDOFS{ii}).globalNodeID);
     for e = 1:nELEM
         bFun = ELEM(e).LbFun;
         % Apply BC
-        [isInElement,idx] = ismember(BC.(DOFS{ii}).globalNodeID,eCONN(:,e));
+        [isInElement,idx] = ismember(BC.(bcDOFS{ii}).globalNodeID,eCONN(:,e));
         if isInElement == true
             dNG = C{e}*ELEM(e).LDerivBasisFuns;
             dNG = formula(dNG);
@@ -52,25 +51,25 @@ for ii = 1:6
                 gTerm = val * numericalQuadrature(integrand,GQ);
             end
             gTerm = formula(gTerm);
-            globalDOF = nodeDOFS(ii,eCONN(A,e));%BC.(DOFS{ii}).globalNodeID);
+            globalDOF = nodeDOFS(ii,eCONN(A,e));%BC.(bcDOFS{ii}).globalNodeID);
             F(globalDOF) = F(globalDOF) - sum(gTerm); % VERIFY
         end
     end
 end
-    
+
 
 
 %% Neumann BCs ( u'(x), v'(x), w'(x) )
 for ii = 4:6
-    val = BC.(DOFS{ii}).val;
-    Loc = BC.(DOFS{ii}).location;
-    BC.(DOFS{ii}).globalNodeID = find(isAlways(subs(Loc,symvar(Loc), nodes)));
-    BC.(DOFS{ii}).globalDOF = nodeDOFS(ii,BC.(DOFS{ii}).globalNodeID);
+    val = BC.(bcDOFS{ii}).val;
+    Loc = BC.(bcDOFS{ii}).location;
+    BC.(bcDOFS{ii}).globalNodeID = find(isAlways(subs(Loc,symvar(Loc), nodes)));
+    BC.(bcDOFS{ii}).globalDOF = nodeDOFS(ii,BC.(bcDOFS{ii}).globalNodeID);
     
     for e = 1:nELEM
         Ne = C{e}*formula(ELEM(e).LBasisFuns);
         % Apply h
-        [isInElement,idx] = ismember(BC.(DOFS{ii}).globalNodeID,eCONN(:,e));
+        [isInElement,idx] = ismember(BC.(bcDOFS{ii}).globalNodeID,eCONN(:,e));
         if isInElement == true
             nLocalNodes = length(ELEM(e).LNodes);
             for n = 1:nLocalNodes
@@ -78,7 +77,7 @@ for ii = 4:6
                     Nh = Ne(n);
                     Nh = symfun(Nh,symvar(Nh)); % Turn into symbolic function
                     hTerm = Nh(ELEM(e).LNodes(n)) * val;
-                    globalDOF = nodeDOFS(ii,eCONN(A,e));%BC.(DOFS{ii}).globalNodeID);
+                    globalDOF = nodeDOFS(ii,eCONN(A,e));%BC.(bcDOFS{ii}).globalNodeID);
                     F(globalDOF) = F(globalDOF) - hTerm;
                 end
             end
